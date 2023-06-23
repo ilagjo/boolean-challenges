3193a16c9e878409f22374139d0fece7f6698598
Ilaria Colagiorgio <81817242+ilagjo@users.noreply.github.com>
2023-06-23 14:06:10 +0200

    updated

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,48 +1,44 @@
 function openModal() {
-    const modal = document.querySelector(".modal");
-    modal.style.display = "block";
-  }
-  
-  function closeModal() {
-    const modal = document.querySelector(".modal");
-    modal.style.display = "none";
-  }
+  const modal = document.querySelector(".modal");
+  modal.style.display = "block";
+}
 
+function closeModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = "none";
+}
 
+const cardsData = [
+  {
+    imgSrc: "./src/images/screen-1.png",
+    imgAlt: "Day One",
+    description: "HTML and CSS, first lesson project, Landing Page",
+    href: "./landing-page/project-1.html",
+  },
+  {
+    imgSrc: "./src/images/screen-2.png",
+    imgAlt: "Quote from a Bot.",
+    description: "Lesson Project covering Javascript",
+    href: "#",
+  },
+  {
+    imgSrc: "./src/images/screen-3.png",
+    imgAlt: "Emoji Chef Project",
+    description: "Lesson Project realized using OpenAI platform",
+    href: "#",
+  },
+  {
+    imgSrc: "./src/images/screen-4.png",
+    imgAlt: "Venture Game Project",
+    description: "Lesson Project realized using OpenAI platform",
+    href: "#",
+  },
+];
 
+function createCards(cardsData) {
+  const container = document.querySelector(".cards");
 
-  const cardsData = [
-    {
-      imgSrc: "./src/images/screen-1.png",
-      imgAlt: "Day One",
-      description: "HTML and CSS, first lesson project, Landing Page",
-      href: "../landing-page/project-1.html",
-    },
-    {
-      imgSrc: "./src/images/screen-2.png",
-      imgAlt: "Quote from a Bot.",
-      description: "Lesson Project covering Javascript",
-      href: '#',
-    },
-    {
-      imgSrc: "./src/images/screen-3.png",
-      imgAlt: "Emoji Chef Project",
-      description: "Lesson Project realized using OpenAI platform",
-      href: "#",
-    },
-    {
-      imgSrc: "../src/images/screen-4.png",
-      imgAlt: "Venture Game Project",
-      description: "Lesson Project realized using OpenAI platform",
-      href: "#",
-    },
-  ];
-  
-  
-  function createCards(cardsData) {
-    const container = document.querySelector(".cards");
-  
-    const cardTemplate = (card) => `
+  const cardTemplate = (card) => `
       <div class="card" style="width: 22rem">
         <div class="card-image">
           <img src="${card.imgSrc}" class="card-img-top" alt="${card.imgAlt}">
@@ -55,37 +51,35 @@ function openModal() {
         </div>
       </div>
     `;
-  
-    const cardsHtml = cardsData.map(cardTemplate).join("");
-  
-    container.innerHTML = cardsHtml;
-  
-    // Just for working progress projects
-    const viewButtons = document.querySelectorAll(".view-btn");
-    for (let i = 0; i < viewButtons.length; i++) {
-      const button = viewButtons[i];
-      button.addEventListener("click", function (event) {
-        event.preventDefault();
-        if (i >= cardsData.length - 3) {
-          openModal();
-        } else {
-          window.location.href = button.getAttribute("href");
-        }
-      });
-    }
-  }
 
-  
-  createCards(cardsData);
-  
-  window.onload = function () {
-    var cards = document.querySelectorAll(".card");
-    for (var i = 0; i < cards.length; i++) {
-      cards[i].classList.add("visible");
-    }
-  };
-  
-  // Aggiungi l'evento click per chiudere il modale
-  const closeBtn = document.querySelector(".close");
-  closeBtn.addEventListener("click", closeModal);
-  
+  const cardsHtml = cardsData.map(cardTemplate).join("");
+
+  container.innerHTML = cardsHtml;
+
+  // Just for working progress projects
+  const viewButtons = document.querySelectorAll(".view-btn");
+  for (let i = 0; i < viewButtons.length; i++) {
+    const button = viewButtons[i];
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      if (i >= cardsData.length - 3) {
+        openModal();
+      } else {
+        window.location.href = button.getAttribute("href");
+      }
+    });
+  }
+}
+
+createCards(cardsData);
+
+window.onload = function () {
+  var cards = document.querySelectorAll(".card");
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].classList.add("visible");
+  }
+};
+
+// Aggiungi l'evento click per chiudere il modale
+const closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", closeModal);
